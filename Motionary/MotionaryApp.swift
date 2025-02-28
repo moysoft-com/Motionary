f756d275d971564f6945abb0bfc5d14d82ecfa37
lian <lianschusterr@icloud.com>
2025-02-28 18:11:10 +0100

    Initial Commit

---
+++ Motionary/MotionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionaryApp.swift
+//  Motionary
+//
+//  Created by Lian on 28.02.25.
+//
+
+import SwiftUI
+
+@main
+struct MotionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MotionaryTests/MotionaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  MotionaryTests.swift
+//  MotionaryTests
+//
+//  Created by Lian on 28.02.25.
+//
+
+import Testing
+@testable import Motionary
+
+struct MotionaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
